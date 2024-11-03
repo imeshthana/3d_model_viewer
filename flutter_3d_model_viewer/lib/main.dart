a0058a3896e5997beda59f4cdf85a76bff402c6f
Imesh Thanapathi <imeshthanapathi@gmail.com>
2024-11-03 21:15:15 +0530

    Flutter app completed

---
+++ flutter_3d_model_viewer/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: '3D Model Viewer',
+      home: MyHomePage(title: '3D Model Viewer'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String qrData = 'Unknown';
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Center(
+          child: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      body: const Center(
+       child: ModelViewer(
+          backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
+          src: 'assets/bmw.glb',
+          alt: 'A 3D model of a car',
+          ar: true,
+          autoRotate: true,
+          disableZoom: true,
+        ),
+      ),
+    );
+  }
+}
